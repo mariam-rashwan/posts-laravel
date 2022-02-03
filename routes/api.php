cd6796da9976ffb9a8a6ebb45ca8fdf55fbe863a
mariam-rashwan <mariamrashwan23@gmail.com>
2022-02-03 05:36:44 +0200

    authantication

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +25,25 @@ Route::get('/test',function(){
     return ' hello from api';
 });
 
-Route::get('posts',[PostController::class,'index']);
+Route::get('posts',[PostController::class,'index'])->middleware('auth:sanctum');
 Route::get('posts/{post}',[PostController::class,'show']);
 Route::post('posts',[PostController::class,'store']);
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
 
